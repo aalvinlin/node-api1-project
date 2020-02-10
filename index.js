0e3ff70b0d030e07658684bfb4df277cdcf618ba
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-10 14:55:39 -0800

    started working on POST request

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 // implement your API here
 const express = require("express");
 const server = express();
-
 const port = 5000;
+
+server.use(express.json())
 
 const db = require("./data/db");
 
 server.listen(port, () => console.log("listening on port", port));
+
+// POST request: /api/users
+server.post("/api/users", (request, response) => {
+
+    const userInfo = request.body
+
+    if (!userInfo.name || !userInfo.bio)
+        { response.status(400).json({ errorMessage: "Please provide name and bio for the user." })}
+    
+    else
+        {
+            db.insert(userInfo)
+                .then(user => {
+                    response.status(201).json(user);
+                })
+                .catch(error => {
+                    response.status(500).json({ errorMessage: "There was an error while saving the user to the database." })
+                })
+        }
+
+    
+})
 
 // GET requests
 server.get("/", (request, response) => {
@@ -52,18 +75,31 @@ server.delete("/api/users/:id", (request, response) => {
 
     const { id } = request.params;
 
-    db.remove(id)
-        .then(data => {
-
+    // first check to see whether user exists
+    db.findById(id)
+        .then(user => {
             // zero means no user found with specified ID; SQLite servers start at index 1
-            if (data === 0)
-                { response.status(404).json({ message: "The user with the specified ID does not exist." }) }
-            // return the contents of the deleted object
+            if (user === 0)
+                {
+                    response.status(404).json({ message: "The user with the specified ID does not exist." })
+                }
+            
+            // user exists; try to delete
             else
-                { response.status(200).json(data); }
+                {
+                    db.remove(id)
+                        // return info of deleted user
+                        .then(data => {
+                            response.status(200).json(user);
+                        })
+                        .catch(error => {
+                            response.status(500).json({ message: "The user information could not be retrieved." })
+                        })
+                }
         })
         .catch(error => {
             response.status(500).json({ message: "The user information could not be retrieved." })
         })
+
     
 })
