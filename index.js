a9fcc4610d6e2fab47b76a9f25054dbe98add93a
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-10 14:12:21 -0800

    GET working

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // implement your API here
+const express = require("express");
+const server = express();
+
+const port = 5000;
+
+const db = require("./data/db");
+
+server.listen(port, () => console.log("listening on port", port));
+
+// GET requests
+server.get("/", (request, response) => {
+    response.json({"message": "Server running on port", port})
+})
+
+// GET request: /api/users
+server.get("/api/users", (request, response) => {
+
+    db.find()
+        .then(data => {
+            response.status(200).json(data);
+        })
+        .catch(error => {
+            response.status(500).json({ errorMessage: "The users information could not be retrieved." })
+        })
+    
+})
+
+// GET request: /api/users/:id
+server.get("/api/users/:id", (request, response) => {
+
+    const { id } = request.params;
+
+    db.findById(id)
+        .then(data => {
+            response.status(200).json(data);
+        })
+        .catch(error => {
+            response.status(404).json({ message: "The user with the specified ID does not exist." })
+        })
+    
+})
